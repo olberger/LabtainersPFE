43cb99340e7e242297318643b37a1101480d9ffe
Ironem <57012015+Ironem@users.noreply.github.com>
2022-01-13 10:03:32 +0100

    Update run-action.sh

--- run-action.sh
+++ run-action.sh
@@ -3,6 +3,6 @@
 curl \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
-  -H "Authorization: token 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" \
+  -H "Authorization: token $1" \
   https://api.github.com/repos/Ironem/LabsPFE/actions/workflows/17417990/dispatches \
-  -d '{"ref":"main" }'
+  -d '{"ref":"main", inputs: {name: $@:2 } }'
