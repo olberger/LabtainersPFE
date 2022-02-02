22e1437693cd5ff9a51e37e55185daa79f9cfd14
IlyesBenighil <60348238+IlyesBenighil@users.noreply.github.com>
2022-02-02 15:53:37 +0100

    Update labtainer.sh

--- script/labtainer.sh
+++ script/labtainer.sh
@@ -1,5 +1,5 @@
 #install docker
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt -y update
